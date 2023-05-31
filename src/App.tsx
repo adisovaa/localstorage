f22fc9d07992d258d88d37fdda873e9619235dec
lorem <79251934+adisovaa@users.noreply.github.com>
2023-05-31 00:04:05 -0400

    04 - UseEffect, использование local storage в React

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,71 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
     const [value, setValue] = useState<number>(0)
 
-    const incHandler = () => {
-        setValue(value + 1)
-    }
 
-    const setToLocalStorage = () => {
-        localStorage.setItem('counterValue', JSON.stringify(value))
-        localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
-    }
-
-    const getToLocalStorage = () => {
+    useEffect(() => {
         let valueAsString = localStorage.getItem('counterValue')
 
         if (valueAsString) {
             let newValue = JSON.parse(valueAsString)
             setValue(newValue)
         }
+    }, [])
+
+
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    }, [value])
+
+    const incHandler = () => {
+        setValue(value + 1)
     }
 
-    const clearLocalStorage = () => {
-        localStorage.clear()
-        setValue(0)
-    }
 
-    const removeLocalStorage = () => {
-        localStorage.removeItem('counterValue + 1')
-    }
+    // const setToLocalStorage = () => {
+    //     localStorage.setItem('counterValue', JSON.stringify(value))
+    //     localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
+    // }
 
-    const setSessionStorage = () => {
-        sessionStorage.setItem('counterValue', JSON.stringify(value))
-    }
+    // const getToLocalStorage = () => {
+    //     let valueAsString = localStorage.getItem('counterValue')
+    //
+    //     if (valueAsString) {
+    //         let newValue = JSON.parse(valueAsString)
+    //         setValue(newValue)
+    //     }
+    // }
+
+    // const clearLocalStorage = () => {
+    //     localStorage.clear()
+    //     setValue(0)
+    // }
+
+    // const removeLocalStorage = () => {
+    //     localStorage.removeItem('counterValue + 1')
+    // }
+
+    // const setSessionStorage = () => {
+    //     sessionStorage.setItem('counterValue', JSON.stringify(value))
+    // }
+
 
     return (
         <div className="App">
             <h1>LocalStorage</h1>
             <h2>{value}</h2>
             <button onClick={incHandler}>inc</button>
-            <button onClick={setToLocalStorage}>setToLocalStorage</button>
-            <button onClick={getToLocalStorage}>getToLocalStorage</button>
-            <button onClick={clearLocalStorage}>clearLocalStorage</button>
-            <button onClick={removeLocalStorage}>clearLocalStorage</button>
-            <br/>
 
-            <button onClick={setSessionStorage}>sessionStorage</button>
+            {/*<button onClick={setToLocalStorage}>setToLocalStorage</button>*/}
+            {/*<button onClick={getToLocalStorage}>getToLocalStorage</button>*/}
+            {/*<button onClick={clearLocalStorage}>clearLocalStorage</button>*/}
+            {/*<button onClick={removeLocalStorage}>clearLocalStorage</button>*/}
+            {/*<br/>*/}
+            {/*<br/>*/}
+            {/*<button onClick={setSessionStorage}>sessionStorage</button>*/}
+
         </div>
     );
 }
